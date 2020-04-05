0ac1581a1ed1be590bf75d4b02b7fc1bbb47fb26
Thomas Shannon <thomasshannon@thomass-mbp.lan>
2020-04-05 16:34:16 -0700

    - updated page 1

--- src/components/CellLine.vue
+++ src/components/CellLine.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="[display]">
-    <span :class="['cell-row-label', display]">{{ label }}</span>
+    <span v-if="label" :class="['cell-row-label', display]">{{ label }}</span>
     <div class="pdf-line" :style="{width: line ? line + 'px' : '100%'}">{{ data }}</div>
     </div>
 </template>
@@ -12,7 +12,7 @@ export default {
         data: {},
         line: {},
         display: {
-            default: 'd-flex'
+            default: 'd-flex space-between'
         }
     },
 }
