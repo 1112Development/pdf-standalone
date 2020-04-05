26939c829856b8f0fd9d3dd3e8f3efbb25bf09fc
Thomas Shannon <thomasshannon@thomass-mbp.lan>
2020-04-04 19:38:05 -0700

    - added config for compiler in vue - added cell row component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './scss/main.scss'
+import Vue from 'vue'
+
+Vue.component('cell-row', {
+    props: {
+        label: {
+
+        },
+        data: {
+
+        }
+    },
+    template: `
+        <div class="d-flex">
+            <span class="cell-row-label">{{ label }}</span>
+            <div class="pdf-line">{{ data }}</div>
+        </div>
+    `
+});
+
+new Vue({
+    el: '#app'
+});
